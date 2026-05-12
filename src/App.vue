ca4669417c8c0bbd258215913e212a6dc9656d1f
boan <boan931004@gmail.com>
2026-05-13 02:19:48 +0800

    上線vercel

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import NavBar from './view/components/NavBar.vue'
 import Footer from './view/components/Footer.vue'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen bg-surface">
     <NavBar />
-    
     <main class="flex-grow pt-[72px] pb-16 md:pb-0 flex flex-col">
       <router-view class="flex-grow" />
     </main>
-    
     <Footer />
   </div>
+  <SpeedInsights />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index'
-
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 const app = createApp(App)
 app.use(router)
+app.component('SpeedInsights', SpeedInsights)
 app.mount('#app')
